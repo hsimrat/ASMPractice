4e331057c7d4da412bbd6aa972ab3d69a158897e
hst <thukralmickey@gmail.com>
2017-06-07 17:39:07 +0530

    PracappRepo

---
+++ PracticeApp/FirstClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApp
+{
+    class Expenditure
+    {
+
+       // private int amount=0;
+       /// private string description;
+       // private DateTime timestamp;
+
+   
+        public int Amount { get; set; }
+        public string Description { get; set; }
+
+        public string TimeStamp { get; set; }
+
+        public Expenditure()
+        {
+            Console.WriteLine("this is default constructor");
+        }
+        //public Expenditure(int amt,string desc , string timestmp)
+        //{
+        //    this.Amount = amt;
+        //    this.Description = desc;
+        //    this.TimeStamp = timestmp;
+
+        //}
+
+        public override string ToString()
+        {
+                return  Amount +", "+ Description +", "+TimeStamp+" ." + "";
+        }
+
+    }
+    public enum ExpenseType
+    {
+        Cash =1,
+        Card=2,
+        NetBanking=3
+        
+    };
+    class ExpenditureByCash : Expenditure
+    {
+      
+        
+    }
+    class ExpenditureByCard
+    { }
+
+    class ExpenditureByFamily
+    {
+        
+    }
+}
---
+++ PracticeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expenditure objExpen = new Expenditure { Amount = 100, Description = "taxi fare", TimeStamp=DateTime.Now.Day+" "+DateTime.Now.ToString("MMM")+" " + DateTime.Now.Year+""};
+           
+            Console.WriteLine("this is amount , Desc and Date time  is : {0} ",objExpen.ToString());
+            //Console.WriteLine("this is description is : {0} ", objExpen.Description);
+            //Console.WriteLine("this is date time is : {0} ", objExpen.TimeStamp);
+            Console.WriteLine(Console.ReadKey());
+        }
+    }
+}
