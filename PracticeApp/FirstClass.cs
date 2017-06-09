7d6c6b3b4e5284cb55b611c03ff74e628a830fd4
hst <thukralmickey@gmail.com>
2017-06-09 18:20:22 +0530

    update 2

--- PracticeApp/FirstClass.cs
+++ PracticeApp/FirstClass.cs
@@ -12,8 +12,9 @@ namespace PracticeApp
        // private int amount=0;
        /// private string description;
        // private DateTime timestamp;
-
-   
+        private static readonly double GSTRate =0.16;
+        private static readonly double CSTRate = 0.00;
+        public static readonly double servicechargesforcash = 0.10;
         public int Amount { get; set; }
         public string Description { get; set; }
 
@@ -30,30 +31,95 @@ namespace PracticeApp
         //    this.TimeStamp = timestmp;
 
         //}
+        protected  double CalculateGST()
+        {
+            return  Amount*GSTRate;
+        }
 
+        protected  double CalculateCST()
+        {
+            return Amount * CSTRate;
+        }
         public override string ToString()
         {
-                return  Amount +", "+ Description +", "+TimeStamp+" ." + "";
+                return  Amount +", "+ Description +", "+TimeStamp + "," + ExpenseType.FoodExpense+".";
+        }
+
+        protected void CheckPaymentType(ref PaymentType objpt)
+        {
+            CashExpenses objCashExp = new CashExpenses();
+
+            switch (objpt)
+            {
+
+                case PaymentType.Cash:
+                    objCashExp.ProcessCashExpenses();
+                    break;
+                case PaymentType.Card:
+                    break;
+                case PaymentType.NetBanking:
+                    break;
+                default:
+                    break;
+            }
+
         }
 
     }
     public enum ExpenseType
     {
-        Cash =1,
-        Card=2,
-        NetBanking=3
+        TravelExpense =1,
+        FoodExpense=2,
+        Misc=3,
+        GroceryExpense=4
+
         
     };
-    class ExpenditureByCash : Expenditure
-    {
-      
-        
-    }
-    class ExpenditureByCard
-    { }
 
-    class ExpenditureByFamily
+    enum TransactionType
     {
+        Payment=1,
+        Receipts=2
+    };
+
+    public enum PaymentType
+    {
+        Cash = 1,
+        Card = 2,
+        NetBanking = 3
+
+    };
+    class CashExpenses : Expenditure
+    {
+
+        protected double CalculateServiceChrgforCash()
+        {
+            return Amount * servicechargesforcash;
+        
+        }
+
+        public double ProcessCashExpenses()
+        {
+
+            return Amount + CalculateGST() + CalculateCST() + CalculateServiceChrgforCash();
+        }
         
     }
+    class CardExpenses : Expenditure
+    {
+
+        static readonly double servicechargesforcard = 0.14;
+
+        private double CalculateServiceChrgforCard()
+        {
+            return Amount * servicechargesforcard;
+
+        }
+        protected double ProcessCardExpenses()
+        {
+
+            return Amount + CalculateGST() + CalculateCST() + CalculateServiceChrgforCard();
+        }
+    }
+
 }
